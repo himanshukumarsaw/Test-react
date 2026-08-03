91944647e41836279c6dea4117426ab326dbec00
himanshukumarsaw <sawmanojkumar65@gmail.com>
2026-08-03 14:52:21 +0530

    2:50

--- Himanshu/src/Layout/AppLayout.jsx
+++ Himanshu/src/Layout/AppLayout.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import { Outlet } from 'react-router-dom'
 const AppLayout = () => {
   return (
-    <div>AppLayout</div>
+    <>
+        <NavBar/>
+        <Outlet/>
+    </>
   )
 }
 
--- Himanshu/src/shared/NavBar.jsx
+++ Himanshu/src/shared/NavBar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <header>
+      <div className='container'>
+          <div className='Logo'>
+            <Link to="/"><h1>Himanshu</h1></Link>
+          </div>
+          <div className='navlist'>
+          <ul>
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/about">Qustion Page</NavLink></li>
+            </ul>
+            </div>
+        </div> 
+    </header>
   )
 }
 
