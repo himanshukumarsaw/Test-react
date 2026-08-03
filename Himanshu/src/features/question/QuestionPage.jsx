46501da87d4dfcb9fd94a96bf15fe70e7219f112
himanshukumarsaw <sawmanojkumar65@gmail.com>
2026-08-03 15:51:36 +0530

    3:50

--- Himanshu/src/features/question/QuestionPage.jsx
+++ Himanshu/src/features/question/QuestionPage.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import QuestionCard from "./component/QuestionCard";
+import QuestionAnswer from "../../Api/QuestionAnswer.json"
 const QuestionPage = () => {
   return (
     <div>
-      <h2>Question:</h2>
+    {/* i am Doing option no B */}
+      <h2>Question:</h2>  
       <div>
-        {
-          
+        {QuestionAnswer.length>0?(
+          QuestionAnswer.map((ques)=>(
+            <div key={questions.id}>
+              <QuestionCard  questions={questions}/>
+            </div>
+          ))
+          ):<p>No answer is available</p>
         }
       </div>
     </div>
---
+++ Himanshu/src/features/question/component/QuestionCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const QuestionCard = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default QuestionCard
