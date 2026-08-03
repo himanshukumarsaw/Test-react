2e1216ba3d372bc5a580857c733834cd545b8e9f
himanshukumarsaw <sawmanojkumar65@gmail.com>
2026-08-03 15:06:38 +0530

    3:00

--- Himanshu/src/features/home/Home.jsx
+++ Himanshu/src/features/home/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import Student from "../../Api/Student.json";
+import { Link } from "react-router-dom";
 const home = () => {
   return (
-    <div>home</div>
-  )
-}
+    <main className="hero-container">
+      <div className="hero-image">
+        <img
+          src="https://imageio.forbes.com/specials-images/imageserve/6262ee8c547b50e80b312206/CrewPhoto1/0x0.jpg?width=960"
+          className="banner-image"
+        />
+      </div>
+      <div className="hero-details">
+        <h2>Student Name:{Student.StudentName}</h2>
+        <h3>Roll Number:{Student.RollNumber}</h3>
+        <h3>Admission Number:{Student.AdmissionNumber}</h3>  Branch Semester College Name
+      </div>
+      <Link to="/questionpage">
+        <button>Start</button>
+      </Link>
+    </main>
+  );
+};
 
-export default home
+export default home;
